3c242823c8dac46bb2d77e2d92aa5e4954fd3e41
jubianchi <contact@jubianchi.fr>
2016-03-11 01:33:14 +0100

    Revert "Do not use %import"
    This reverts commit 93297137ea577466e4aeca3dc4caffe84ad2a91f.

--- tests/tml.pp
+++ tests/tml.pp
@@ -1,22 +1,4 @@
-%skip              T_SPACE                 \s
-
-%token             T_OP_PLUS               plus
-%token             T_OP_MINUS              moins
-%token             T_OP_MULTI              multiplié par
-%token             T_OP_DIVIDE             divisé par
-%token             T_OP_EQUAL              égal
-
-%token             T_FN                    \.[a-zA-Z_][a-zA-Z0-9_]*
-%token             T_VAR                   @[a-zA-Z_][a-zA-Z0-9_]*
-%token             T_NUMBER                \-?[1-9][0-9]*
-
-%token             T_QUOTE                 "                        -> quoted
-%token      quoted:T_QUOTE                 "                        -> default
-%token             T_OPEN_PAREN            \(
-%token             T_CLOSE_PAREN           \)
-%token             T_COMMA                 ,
-
-%token      quoted:T_CHAR                  \\"|[^"]
+%import ../src/tml.pp
 
 #tml:
       expr()
@@ -25,15 +7,6 @@
 
 #expr:
     <T_NUMBER> ( operator() expr() | division() )?
-
-#assign:
-    lvar() ::T_OP_EQUAL:: rval()
-
-#str:
-    ::T_QUOTE:: <T_CHAR>* ::T_QUOTE::
-
-#lvar:
-    <T_VAR>
 
 // Avoids division by zero
 division:
